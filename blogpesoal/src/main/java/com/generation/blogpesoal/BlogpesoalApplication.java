2363ca983ddc29c8f521e105661f8c64bb97d166
Silvia Santana <oliversilvia20@gmail.com>
2022-10-03 20:34:13 -0300

    Classes

---
+++ blogpesoal/src/main/java/com/generation/blogpesoal/BlogpesoalApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogpesoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpesoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpesoalApplication.class, args);
+	}
+
+}
---
+++ blogpesoal/src/test/java/com/generation/blogpesoal/BlogpesoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.blogpesoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpesoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
